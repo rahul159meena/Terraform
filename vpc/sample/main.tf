03dbf74659ba2232ca7216befb41d93be268fd84
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-21 18:06:56 +0530

    vpc name added

--- vpc/sample/main.tf
+++ vpc/sample/main.tf
@@ -4,4 +4,5 @@ module "vpc" {
     instance_tenancy     = var.instance_tenancy
     enable_dns_support   = var.enable_dns_support
     enable_dns_hostnames = var.enable_dns_hostnames
+    name                 = var.name
 }
