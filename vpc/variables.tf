3e0e6d8e28264ce0ef8ba73b0ce880500cde91ee
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-21 18:00:40 +0530

    variable file indentation fixed

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,40 +1,34 @@
 # Variable for VPC
-variable "cidr_block" 
-{
+variable "cidr_block" {
     description = "The CIDR Block for VPC"
     type        = string
     default     = "10.0.0.0/16"
 }
 
-variable "instance_tenancy"
-{
+variable "instance_tenancy" {
     description = "A tenancy option for instances launched into the VPC"
     type        =  string
     default     = "default"
 }
 
-variable "enable_dns_support"
-{
+variable "enable_dns_support" {
     description = "A boolean flag to enable/disable DNS support in the VPC"
     type        = bool
     default     = true
 }
 
-variable "enable_dns_hostnames"
-{
+variable "enable_dns_hostnames" {
     description = "A boolean flag to enable/disable DNS hostnames in the VPC"
     type        = bool
     default     = true
 }
 
-variable "name"
-{
+variable "name" {
     description = "The name for the VPC"
     type        = string
 }
 
-variable "tags"
-{
+variable "tags" {
     description = "A mapping of tags to assign to all resources"
     type        = map(string)
     default     = {}
