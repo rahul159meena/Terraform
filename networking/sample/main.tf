ae7b82ac48d896c75c66c62b84c9748811d1b71b
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-22 12:50:41 +0530

    vpc moved to networking

---
+++ networking/sample/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+    source               = "git://github.com/PratapSingh13/Terraform.git//vpc"
+    cidr_block           = var.cidr_block
+    instance_tenancy     = var.instance_tenancy
+    enable_dns_support   = var.enable_dns_support
+    enable_dns_hostnames = var.enable_dns_hostnames
+    name                 = var.name
+}
