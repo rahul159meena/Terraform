01bfe78418c317b6d9644133c916e87576302166
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-23 10:39:21 +0530

    name changes

--- networking/main.tf
+++ networking/main.tf
@@ -3,10 +3,14 @@ resource "aws_vpc" "vpc" {
     instance_tenancy     = var.instance_tenancy
     enable_dns_support   = var.enable_dns_support
     enable_dns_hostnames = var.enable_dns_hostnames
-    tags                 = merge({ "Name" = var.vpc_name }, var.tags)
+    tags {
+        Name = var.vpc_name
+    }
 }
 
 resource "aws_internet_gateway" "igw" {
     vpc_id = var.vpc_id
-    tags   = merge({ "Name" = var.igw_name }, var.tags)
+    tags {
+        Name = var.igw_name
+    }
 }
