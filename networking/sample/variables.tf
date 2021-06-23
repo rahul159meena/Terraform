315e80d9d679bfb85d01036cc37086fecf71a321
Yogendra Pratap Singh <42297673+PratapSingh13@users.noreply.github.com>
2021-06-23 13:38:43 +0530

    subnet cidr changed

--- networking/sample/variables.tf
+++ networking/sample/variables.tf
@@ -58,7 +58,7 @@ variable "public_availability_zones" {
 variable "public_subnet_cidr_blocks" {
     description = "The CIDR blocks for the public subnets"
     type        = list(string)
-    default     = ["10.255.0.0/24", "10.255.1.0/24"]
+    default     = ["10.0.0.0/28", "10.0.0.16/28"]
 }
 
 variable "map_public_ip_on_launch" {
